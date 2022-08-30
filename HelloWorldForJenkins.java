33afbcf690dde911e99b980f8964923ae144531f
vamshikrishna-22 <104588173+vamshikrishna-22@users.noreply.github.com>
2022-08-30 13:16:12 +0530

    Update HelloWorldForJenkins.java

--- HelloWorldForJenkins.java
+++ HelloWorldForJenkins.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class HelloWorldForJenkins {
 	public static void main(String[] args) {
-		System.out.println(".....*****HelloWorld from Jenkins*****......");
+		System.out.println(".....*****HelloWorld from Jenkins and made some changes*****......");
 	}
 }
